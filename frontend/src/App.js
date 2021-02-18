361bed3340ba619d8db263e4f7f8b5a4bd2791f1
chan-cheyE <kchandran024@gmail.com>
2021-02-18 10:23:46 +0530

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Menubar from './Components/Menubar/Menubar';
 import HomeScreenRoute from '../src/HomeScreen/HomeScreenRoute/HomeScreenRoute';
 import {
-  BrowserRouter as Router,
+  MemoryRouter,
   Switch,
   Route,
 } from "react-router-dom";
@@ -13,14 +13,14 @@ import Footer from './Components/Footer/Footer';
 export default class App extends Component {
   render() {
     return (
-      <Router>
+      <MemoryRouter>
         <Menubar />
         <Switch>
           <Route path="/" component={HomeScreenRoute} exact />
           <Route path="/ContactUs" render={() => (<ContactForm />)} />
         </Switch>
         <Footer />
-      </Router>
+      </MemoryRouter>
     )
   }
 }
